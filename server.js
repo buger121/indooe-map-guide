df6bd0a4d30a7ca59745a69a3cd1d1aed512151c
jinz <jinz2042@gmail.com>
2019-08-08 09:38:23 +0800

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 //解决跨域
 app.all('*',function (req, res, next) {
