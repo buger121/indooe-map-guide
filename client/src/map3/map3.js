a26f3c58ca3f8be2efb836e6f0248a6ef7719177
jinz <jinz2042@gmail.com>
2019-12-21 14:58:03 +0800

    add map3

---
+++ client/src/map3/map3.js
@@ -0,0 +1,21 @@
+import mapCtrl from '../component/map';
+//引入css文件
+import '../style.css';
+
+var map = new esmap.ESMap({
+  container: document.getElementById('map-container'), //地图显示容器
+  token: '20191219', //打开地图对应的token
+  mapDataSrc: 'data/', //地图数据位置，与推荐目录相同可省略
+  mapThemeSrc: 'data/theme/', //地图主题位置，与推荐目录相同可省略
+  visibleFloors: [1, 2], //初始显示楼层
+  focusFloor: 1 //初始聚焦楼层
+});
+map.openMapById(20191219); //根据ID打开地图
+
+mapCtrl.mapConfig(map);
+mapCtrl.mapDraw(map);
+
+const startnav = document.querySelector('#startnav');
+startnav.addEventListener('click', function() {
+  mapCtrl.navigation(map);
+});
--- server.js
+++ server.js
@@ -1,59 +1,68 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-const fs = require('fs')
-const bodyParser = require('body-parser')
-const path = require('path')
-import dbBase from './dbBase'
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const path = require('path');
+import dbBase from './dbBase';
 //连接数据库
-dbBase.connect()
-
+dbBase.connect();
 
 //解决跨域
-app.all('*',function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  
-    if (req.method == 'OPTIONS') {
-      res.send(200); /让options请求快速返回/
-    }
-    else {
-      next();
-    }
-  });
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild'
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+    /让options请求快速返回/;
+  } else {
+    next();
+  }
+});
 
 //处理请求数据
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(express.static('client'))
+app.use(express.static('client'));
 
 //安卓发送定位信息，终点信息
-app.post('/info',(req, res) => {
-  dbBase.insert(req.body).then(function(value){
-    res.send(value)
-  })
-})
+app.post('/info', (req, res) => {
+  dbBase.insert(req.body).then(function(value) {
+    res.send(value);
+  });
+});
 
-app.get('/info',(req, res) => {
-  dbBase.find().then(function(value){
-    res.send(value)
-  })
-})
+app.get('/info', (req, res) => {
+  dbBase.find().then(function(value) {
+    res.send(value);
+  });
+});
 
 app.get('/map1', (req, res, next) => {
   var option = {
     root: path.join(__dirname, 'client/build')
-  }
-  res.sendFile('map1.html', option)
-})
+  };
+  res.sendFile('map1.html', option);
+});
 
 app.get('/map2', (req, res, next) => {
   var option = {
     root: path.join(__dirname, 'client/build')
-  }
-  res.sendFile('map2.html', option)
-})
+  };
+  res.sendFile('map2.html', option);
+});
 
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+app.get('/map3', (req, res, next) => {
+  var option = {
+    root: path.join(__dirname, 'client/build')
+  };
+  res.sendFile('map3.html', option);
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
